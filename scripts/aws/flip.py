76ac1a1ea076df57a144a9aa1a58f12b51a52eac
e0d <ed@edx.org>
2016-04-22 23:44:45 -0400

    pep8 cleanup

--- scripts/aws/flip.py
+++ scripts/aws/flip.py
@@ -8,6 +8,7 @@ import logging
 import botocore.session
 import botocore.exceptions
 
+
 def get_live_stage(client, api_base):
     """Get the current live stage tied to this base path."""
     try:
@@ -18,10 +19,11 @@ def get_live_stage(client, api_base):
         logging.error('No mapping found for "%s", have you bootstrapped it yet?', api_base)
         sys.exit(1)
 
-    logging.info('Found existing base path mapping for API ID "%s", stage "%s"',\
-        mapping['restApiId'], mapping['stage'])
+    logging.info('Found existing base path mapping for API ID "%s", stage "%s"',
+                 mapping['restApiId'], mapping['stage'])
 
     return mapping['stage']
+
 
 def update_base_path_mapping(client, api_base, stage_name):
     """Flip base path pointer to new gateway object so it starts receiving real traffic."""
@@ -31,7 +33,7 @@ def update_base_path_mapping(client, api_base, stage_name):
             domainName=api_base,
             basePath='(none)',
             patchOperations=[
-                {'op':'replace', 'path':'/stage', 'value': stage_name}
+                {'op': 'replace', 'path': '/stage', 'value': stage_name}
             ])
     except botocore.exceptions.ClientError:
         logging.error('Stage "%s" does not yet exist. Aborting operation.', stage_name)
@@ -39,6 +41,7 @@ def update_base_path_mapping(client, api_base, stage_name):
 
     logging.info('API Gateway domain "%s" updated to "%s"', api_base, stage_name)
     return base_path_update
+
 
 def main(log_level=20):
     """Update an AWS API Gateway object from a Swagger configuration file."""
@@ -62,6 +65,7 @@ def main(log_level=20):
     # Flip the base path pointer to the new stage
     else:
         update_base_path_mapping(apig, api_base, next_stage)
+
 
 if __name__ == '__main__':
     main()
